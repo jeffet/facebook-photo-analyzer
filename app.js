0c4a67e9b47465f422b59a051fdb56615a2c4c4f
Jeff Tennenbaum <tennenjl@us.ibm.com>
2017-03-01 13:29:12 -0500

    Adding Debug messages

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 var express = require('express'),
-  app = express(),
-  config = require("./config")(),
-  passport = require('passport'),
-  bodyParser = require('body-parser'),
-  cookieParser = require('cookie-parser'),
-  session = require('express-session'),
-  methodOverride = require('method-override'),
-  watson = require('watson-developer-cloud'),
-  FacebookStrategy = require('passport-facebook').Strategy,
-  _ = require("underscore"),
-  cfenv = require("cfenv"),
-  async = require("async"),
-  graph = require('fbgraph'),
-  request = require('request'),
-  AlchemyApi = require('alchemy-api'),
-  fs = require("fs"),
-  path = require("path"),
-  os = require("os"),
-  Cloudant = require('cloudant');
+    app = express(),
+    config = require("./config")(),
+    passport = require('passport'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    methodOverride = require('method-override'),
+    watson = require('watson-developer-cloud'),
+    FacebookStrategy = require('passport-facebook').Strategy,
+    _ = require("underscore"),
+    cfenv = require("cfenv"),
+    async = require("async"),
+    graph = require('fbgraph'),
+    request = require('request'),
+    AlchemyApi = require('alchemy-api'),
+    fs = require("fs"),
+    path = require("path"),
+    os = require("os"),
+    Cloudant = require('cloudant');
 
 //---Deployment Tracker---------------------------------------------------------
 require("cf-deployment-tracker-client").track();
@@ -26,21 +26,21 @@ require("cf-deployment-tracker-client").track();
 //---Environment Vars-----------------------------------------------------------
 var vcapLocal = null
 try {
-  vcapLocal = require("./vcap-local.json")
-}
-catch (e) {}
+    vcapLocal = require("./vcap-local.json")
+} catch (e) {}
 
-var appEnvOpts = vcapLocal ? {vcap:vcapLocal} : {}
+var appEnvOpts = vcapLocal ? { vcap: vcapLocal } : {}
 var appEnv = cfenv.getAppEnv(appEnvOpts);
+console.log("JT ABOUT TO get Service Creds");
 
 // Retrieves service credentials for the input service
 function getServiceCreds(appEnv, serviceName) {
-  var serviceCreds = appEnv.getServiceCreds(serviceName)
-  if (!serviceCreds) {
-    console.log("service " + serviceName + " not bound to this application");
-    return null;
-  }
-  return serviceCreds;
+    var serviceCreds = appEnv.getServiceCreds(serviceName)
+    if (!serviceCreds) {
+        console.log("service " + serviceName + " not bound to this application");
+        return null;
+    }
+    return serviceCreds;
 }
 
 //---Set up Watson Personality Insights-----------------------------------------
@@ -53,9 +53,10 @@ var alchemy = new AlchemyApi(process.env.ALCHEMY_API_KEY);
 
 //---Set up Cloudant------------------------------------------------------------
 var cloudantCreds = getServiceCreds(appEnv, "cloudant-photo-analyzer"),
-  dbName = "photo-analyzer",
-  cloudant,
-  db;
+    dbName = "photo-analyzer",
+    cloudant,
+    db;
+console.log("JT CLoudant Creds: " + JSON.stringify(cloudantCredsd));
 
 //---Routers and View Engine----------------------------------------------------
 app.set('views', __dirname + '/views');
@@ -64,237 +65,229 @@ app.use(cookieParser());
 app.use(bodyParser());
 app.use(methodOverride());
 app.use(session({
-  secret: 'keyboard cat'
+    secret: 'keyboard cat'
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(__dirname + '/public'));
 
 //---auth stuff-----------------------------------------------------------------
-passport.serializeUser(function (user, done) {
-  done(null, user);
+passport.serializeUser(function(user, done) {
+    done(null, user);
 });
 
-passport.deserializeUser(function (obj, done) {
-  done(null, obj);
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
 });
 
 if (process.env.FACEBOOK_APP_ID !== undefined && process.env.FACEBOOK_APP_SECRET !== undefined) {
 
-  passport.use(new FacebookStrategy({
-      clientID: process.env.FACEBOOK_APP_ID,
-      clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: appEnv.url + "/auth/facebook/callback",
-      scope: ["user_photos", "user_posts"]
-    },
-    function(accessToken, refreshToken, profile, done) {
-      async.waterfall([
-        function  (next) {
-          graph.setAccessToken(accessToken);
-          graph.get("/me/photos", next);
+    passport.use(new FacebookStrategy({
+            clientID: process.env.FACEBOOK_APP_ID,
+            clientSecret: process.env.FACEBOOK_APP_SECRET,
+            callbackURL: appEnv.url + "/auth/facebook/callback",
+            scope: ["user_photos", "user_posts"]
         },
-        function (photos, next) {
-          var params = { fields: "images" };
-          _.each(photos.data, function(photo) {
-            photo.graph = graph;
-            photo.userId = profile.id;
-          });
-          console.log("analyzing",photos.data.length, "photos");
-          async.each(photos.data, analyzePhoto, next);
+        function(accessToken, refreshToken, profile, done) {
+            async.waterfall([
+                function(next) {
+                    graph.setAccessToken(accessToken);
+                    graph.get("/me/photos", next);
+                },
+                function(photos, next) {
+                    var params = { fields: "images" };
+                    _.each(photos.data, function(photo) {
+                        photo.graph = graph;
+                        photo.userId = profile.id;
+                    });
+                    console.log("analyzing", photos.data.length, "photos");
+                    async.each(photos.data, analyzePhoto, next);
+                }
+            ], function(error, result) {
+                if (error) {
+                    console.log(error);
+                    done(error);
+                } else {
+                    done(null, profile);
+                }
+            });
         }
-        ], function (error, result) {
-          if (error) {
-            console.log(error);
-            done(error);
-          }
-          else {
-            done(null, profile);
-          }
-        }
-      );
-    }
-  ));
+    ));
 }
 
 app.get('/auth/facebook', passport.authenticate('facebook'));
 
 app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', {
-    successRedirect: '/',
-    failureRedirect: '/login'
-  })
+    passport.authenticate('facebook', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    })
 );
 
 //---Start the server, conenct to cloudant--------------------------------------
 app.listen(appEnv.port, function() {
-  console.log("server started on port " + appEnv.port);
-  var dbCreated = false;
-  Cloudant({account:cloudantCreds.username, password:cloudantCreds.password}, function(er, dbInstance) {
-      cloudant = dbInstance;
-      if (er) {
-          return console.log('Error connecting to Cloudant account %s: %s', cloudantCreds.username, er.message);
-      }
+    console.log("server started on port " + appEnv.port);
+    var dbCreated = false;
+    Cloudant({ account: cloudantCreds.username, password: cloudantCreds.password }, function(er, dbInstance) {
+        cloudant = dbInstance;
+        if (er) {
+            return console.log('Error connecting to Cloudant account %s: %s', cloudantCreds.username, er.message);
+        }
 
-      console.log('Connected to cloudant');
-      cloudant.ping(function(er, reply) {
-          if (er) {
-              return console.log('Failed to ping Cloudant. Did the network just go down?');
-          }
+        console.log('Connected to cloudant');
+        cloudant.ping(function(er, reply) {
+            if (er) {
+                return console.log('Failed to ping Cloudant. Did the network just go down?');
+            }
 
-          console.log('Server version = %s', reply.version);
-          console.log('I am %s and my roles are %j', reply.userCtx.name, reply.userCtx.roles);
+            console.log('Server version = %s', reply.version);
+            console.log('I am %s and my roles are %j', reply.userCtx.name, reply.userCtx.roles);
 
-          cloudant.db.list(function(er, all_dbs) {
-              if (er) {
-                  return console.log('Error listing databases: %s', er.message);
-              }
+            cloudant.db.list(function(er, all_dbs) {
+                if (er) {
+                    return console.log('Error listing databases: %s', er.message);
+                }
 
-              console.log('All my databases: %s', all_dbs.join(', '));
+                console.log('All my databases: %s', all_dbs.join(', '));
 
-              _.each(all_dbs, function(name) {
-                  if (name === dbName) {
-                      dbCreated = true;
-                  }
-              });
-              if (dbCreated === false) {
-                  cloudant.db.create(dbName, seedDB);
-              }
-              else {
-                  db = cloudant.db.use(dbName);
-                  console.log("DB", dbName, "is already created");
-              }
-          });
-      });
-  });
+                _.each(all_dbs, function(name) {
+                    if (name === dbName) {
+                        dbCreated = true;
+                    }
+                });
+                if (dbCreated === false) {
+                    cloudant.db.create(dbName, seedDB);
+                } else {
+                    db = cloudant.db.use(dbName);
+                    console.log("DB", dbName, "is already created");
+                }
+            });
+        });
+    });
 });
 
 //--Analyze a photo from Facebook with Watson and AlchemyAPI--------------------
 function analyzePhoto(photo, callback) {
-  var graph = photo.graph,
-    file = path.join(os.tmpdir(), photo.id + ".jpg"),
-    response = {
-      photoId: photo.id,
-      userId: photo.userId
-    };
-
-  async.waterfall([
-    function (next) {
-      var params = { fields: "images" };
-      graph.get("/" + photo.id, params, next);
-    },
-    function (result, next) {
-      response.source = result.images[0].source;
-      var stream = request.get(result.images[0].source).pipe(fs.createWriteStream(file));
-      stream.on("finish", function() {
-        var params = {
-          image_file: fs.createReadStream(file)
+    var graph = photo.graph,
+        file = path.join(os.tmpdir(), photo.id + ".jpg"),
+        response = {
+            photoId: photo.id,
+            userId: photo.userId
         };
 
-        visualRecognition.recognize(params, function (error, result) {
-          if (error) {
-            console.log(error);
-            callback(null);
-            return;
-          }
-          next(null, result)
-        });
-      });
-    },
-    function (result, next) {
-      response.labels = result.images[0].labels;
-      graph.get("/" + photo.id + "/comments", next);
-    },
-    function (comments, next) {
-      var commentsString = "";
+    async.waterfall([
+        function(next) {
+            var params = { fields: "images" };
+            graph.get("/" + photo.id, params, next);
+        },
+        function(result, next) {
+            response.source = result.images[0].source;
+            var stream = request.get(result.images[0].source).pipe(fs.createWriteStream(file));
+            stream.on("finish", function() {
+                var params = {
+                    image_file: fs.createReadStream(file)
+                };
 
-      _.each(comments.data, function(comment) {
-        commentsString += " " + comment.message
-      });
+                visualRecognition.recognize(params, function(error, result) {
+                    if (error) {
+                        console.log(error);
+                        callback(null);
+                        return;
+                    }
+                    next(null, result)
+                });
+            });
+        },
+        function(result, next) {
+            response.labels = result.images[0].labels;
+            graph.get("/" + photo.id + "/comments", next);
+        },
+        function(comments, next) {
+            var commentsString = "";
 
-      if (commentsString != "") {
-        alchemy.sentiment(commentsString, {}, next);
-      }
-      else {
-        next(null, null);
-      }
+            _.each(comments.data, function(comment) {
+                commentsString += " " + comment.message
+            });
 
-    },
-    function (result, next) {
-      if (result !== null) {
-        response.sentiment = result.docSentiment;
-      }
+            if (commentsString != "") {
+                alchemy.sentiment(commentsString, {}, next);
+            } else {
+                next(null, null);
+            }
 
-      response.type = "photo";
-      db.view("photos", "photo", { keys: [response.photoId]}, next);
-    },
-    function (result, headers, next) {
-        if (result.rows.length === 0) {
-            db.insert(response, next);
+        },
+        function(result, next) {
+            if (result !== null) {
+                response.sentiment = result.docSentiment;
+            }
+
+            response.type = "photo";
+            db.view("photos", "photo", { keys: [response.photoId] }, next);
+        },
+        function(result, headers, next) {
+            if (result.rows.length === 0) {
+                db.insert(response, next);
+            } else {
+                next(null, result);
+            }
         }
-        else {
-          next(null, result);
-        }
-    }
-  ], callback);
+    ], callback);
 }
 
 //---Route handle for the default entry point to the app------------------------
-app.get('/', function (request, response) {
-  var setup = false;
-  if (process.env.FACEBOOK_APP_ID !== undefined && process.env.FACEBOOK_APP_SECRET !== undefined && process.env.ALCHEMY_API_KEY !== undefined) {
-    setup = true;
-  }
-  var opts = {
-    user: request.user,
-    setup: setup
-  };
+app.get('/', function(request, response) {
+    var setup = false;
+    if (process.env.FACEBOOK_APP_ID !== undefined && process.env.FACEBOOK_APP_SECRET !== undefined && process.env.ALCHEMY_API_KEY !== undefined) {
+        setup = true;
+    }
+    var opts = {
+        user: request.user,
+        setup: setup
+    };
 
-  if (request.user) {
-    db.view("photos", "user", { keys: [request.user.id]}, function (error, result) {
-      opts.photos = result.rows;
-      response.render('index', opts);
-    });
-  }
-  else {
-    response.render('index', opts);
-  }
+    if (request.user) {
+        db.view("photos", "user", { keys: [request.user.id] }, function(error, result) {
+            opts.photos = result.rows;
+            response.render('index', opts);
+        });
+    } else {
+        response.render('index', opts);
+    }
 
 });
 
 //--Route handle for logging out------------------------------------------------
-app.get('/logout', function (request, response) {
-  request.logout();
-  response.redirect('/');
+app.get('/logout', function(request, response) {
+    request.logout();
+    response.redirect('/');
 });
 
 //---Seed the db----------------------------------------------------------------
 function seedDB(callback) {
-  db = cloudant.use(dbName);
+    db = cloudant.use(dbName);
 
-  async.waterfall([
-    function (next) {
-      var designDocs = [
-          {
-            _id: '_design/photos',
-            views: {
-              all: {
-                map: function (doc) { if (doc.type === 'photo') { emit(doc._id, doc); } }
-              },
-              user: {
-                map: function (doc) { if (doc.type === 'photo') { emit(doc.userId, doc); } }
-              },
-              photo: {
-                map: function (doc) { if (doc.type === 'photo') { emit(doc.photoId, doc); } }
-              }
-            }
-          }
-     ];
+    async.waterfall([
+        function(next) {
+            var designDocs = [{
+                _id: '_design/photos',
+                views: {
+                    all: {
+                        map: function(doc) { if (doc.type === 'photo') { emit(doc._id, doc); } }
+                    },
+                    user: {
+                        map: function(doc) { if (doc.type === 'photo') { emit(doc.userId, doc); } }
+                    },
+                    photo: {
+                        map: function(doc) { if (doc.type === 'photo') { emit(doc.photoId, doc); } }
+                    }
+                }
+            }];
 
-      async.each(designDocs, db.insert, next);
-    },
-    function (next) {
-      console.log("Created DB", dbName, "and populated it with initial purchases");
-      next();
-    }
-  ], callback)
+            async.each(designDocs, db.insert, next);
+        },
+        function(next) {
+            console.log("Created DB", dbName, "and populated it with initial purchases");
+            next();
+        }
+    ], callback)
 }
